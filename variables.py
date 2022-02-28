35781a40a2f63880ceb22c99b8244f8e450e562d
ThomasCza <czarnecki.thomas@orange.fr>
2022-02-28 16:02:29 +0100

    Création du repo
    ajout des premiers chapitres de cours.
    Ajout des exo correspondants.

---
+++ variables.py
@@ -0,0 +1,50 @@
+# foo
+# bar
+# baz
+
+
+nombre = 123
+print(nombre)
+
+nombre = 3.14
+print(nombre)
+
+nombre = "foo bar baz"
+print(nombre)
+
+nombre = "123"
+print(nombre)
+
+text = "lorem ipsum"
+print(text)
+
+is_day = True
+print(is_day)
+
+has_sugar = False
+print(has_sugar)
+
+has_accepted_ula = None
+print(has_accepted_ula)
+
+html_code = '<h1>Titre de mon blog</h1>'
+print(html_code)
+
+nickname = "John \"Dead\"Doe"
+print(nickname)
+
+multiline_text = "foo\nbar\nbaz"
+print(multiline_text)
+
+multiline_text = """foo
+bar
+baz"""
+print(multiline_text)
+
+
+
+
+
+
+
+
